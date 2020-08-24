f051c16e248d29348c60a0aa2bdd797f138420c3
huong-mac <huong-mac@dimage.co.jp>
2020-08-24 14:16:24 +0700

    add css icon

--- qlvn-client/src/global.js
+++ qlvn-client/src/global.js
@@ -5,7 +5,7 @@
  Version: 1.0
  Author: HuongMV
  ======================================================================================= */
-// import './assets/css/vendor/all.min.css'
+ import './assets/vendor/fontawesome-free/css/all.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'jquery/dist/jquery.min.js'
 import 'jquery.easing/jquery.easing.min.js'
