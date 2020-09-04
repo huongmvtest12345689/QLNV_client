b7c7800f2624777261ef079c49797f34c33ba3bd
unknown <hoang-do@dimage.co.jp>
2020-09-04 15:00:27 +0700

    form validation

--- qlvn-client/src/main.js
+++ qlvn-client/src/main.js
@@ -11,10 +11,12 @@ import Toolbar from 'primevue/toolbar';
 import ToastService from 'primevue/toastservice';
 import Toast from 'primevue/toast';
 import Fieldset from 'primevue/fieldset';
-
+import { ValidationProvider } from 'vee-validate/dist/vee-validate.full.esm';
+Vue.component('ValidationProvider', ValidationProvider);
 /** Global Components */
 import './global.js'
 import i18n from './i18n.js'
+
 Vue.config.productionTip = false
 Vue.component('DataTable', DataTable);
 Vue.component('Column', Column);
@@ -24,6 +26,7 @@ Vue.component('FileUpload', FileUpload);
 Vue.component('Toolbar', Toolbar);
 Vue.component('Toast', Toast);
 Vue.component('Fieldset', Fieldset);
+
 Vue.use(ToastService);
 new Vue({
   router,
--- qlvn-client/src/views/admin/register/Register.vue
+++ qlvn-client/src/views/admin/register/Register.vue
@@ -1,36 +1,68 @@
 <template >
 <div class = "container" >
-    <div class="card o-hidden border-0 shadow-lg my-5 ">
-      <div class="card-body p-0">
+    
+    <div class="card o-hidden border-0 shadow-lg my-5 mx-5 col-lg-8 ">
+      <div class="card-body p-5">
         <!-- Nested Row within Card Body -->
         <div class="row">
-          <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
-          <div class="col-lg-7">
-            <div class="p-5">
-              <div class="text-center">
+          <!-- <div>
+            <img src="../../../assets/ds_logomark.png" alt="logo"  class="col-lg-5 d-none d-lg-block " >
+          </div> -->
+          <!-- <div class = "col-lg-2"></div> -->
+          <div class="col-lg-12">
+            <div class="p-0">
+              <div class="text-center mb-5">
                 <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
               </div>
               <form class="user" v-on:submit.prevent = "createUser">
                 
                  <div class="form-group">
-                  <input type="text" class="form-control form-control-user" id="name" placeholder="Full name" v-model="name">
-                  {{this.name}}
+                   <ValidationProvider rules="required" v-slot="{ errors }" name = "'Fullname'">
+                    <input type="text" class="form-control form-control-user" id="name" placeholder="Full name" v-model="name">
+                      <ul>
+                          <li v-for="error in errors" v-bind:key="error">* {{ error }}</li>
+                      </ul>
+                  </ValidationProvider>
+                  <!-- {{this.name}} -->
                 </div>
                  <div class="form-group">
-                  <input type="text" class="form-control form-control-user" id="email" placeholder="Email Address" v-model="email" v-validate="'required|email'">
-                  {{this.email}}
+                   <ValidationProvider rules="required|email" v-slot="{ errors }" name = "'Email Address'">
+                      <input type="text" class="form-control form-control-user" id="email" placeholder="Email Address" v-model="email">
+                      <ul>
+                        <li v-for="error in errors" v-bind:key="error">* {{ error }}</li>
+                      </ul>
+                  </ValidationProvider>
+                  <!-- {{this.email}} -->
                 </div>
                 <div class="form-group">
-                  <input type="phone" class="form-control form-control-user" id="phone" placeholder="Phone" v-model="phone">
-                  {{this.phone}}
+                   <ValidationProvider rules="required|numeric|max:10" v-slot="{ errors }" name = "'Phone'">
+                    <input type="phone" class="form-control form-control-user" id="phone" placeholder="Phone" v-model="phone">
+                      <ul>
+                          <li v-for="error in errors" v-bind:key="error">* {{ error }}</li>
+                      </ul>
+                  </ValidationProvider>
+                  <!-- <input type="phone" class="form-control form-control-user" id="phone" placeholder="Phone" v-model="phone"> -->
+                  <!-- {{this.phone}} -->
                 </div>
                 <div class="form-group row">
                   <div class="col-sm-6 mb-3 mb-sm-0">
-                    <input type="password" class="form-control form-control-user" id="password" placeholder="Password" v-model="password">
-                    {{this.password}}
+                    <ValidationProvider rules="required" v-slot="{ errors }" name = "'Password'">
+                      <input type="password" class="form-control form-control-user" id="password" placeholder="Password" v-model="password">
+                        <ul>
+                            <li v-for="error in errors" v-bind:key="error">* {{ error }}</li>
+                        </ul>
+                    </ValidationProvider>
+                    
+                    <!-- {{this.password}} -->
                   </div>
                   <div class="col-sm-6">
-                    <input type="password" class="form-control form-control-user" id="retype_password" placeholder="Repeat Password" v-model="retype_password">
+                    <ValidationProvider rules="required" v-slot="{ errors }" name = "'Retype Password'">
+                      <input type="password" class="form-control form-control-user" id="retype_password" placeholder="Repeat Password" v-model="retype_password">
+                        <ul>
+                            <li v-for="error in errors" v-bind:key="error">* {{ error }}</li>
+                        </ul>
+                    </ValidationProvider>
+                    
                   </div>
                 </div>
                 <input type = "submit" class="btn btn-primary btn-user btn-block" value="Register"> 
@@ -67,12 +99,16 @@ export default {
             password:'',
             phone:'',
             retype_password:'',
-            display_order:1
+            display_order:1,
+            errors:[],
         }
     },
     methods:{
         createUser: function(){
-          
+            if(!this.errors){
+              alert(this.errors);
+              return;
+            }
             axios
             .post("http://localhost:8088/register",{
                 username: this.name,
@@ -94,4 +130,20 @@ export default {
 
 <style scoped>
 
+ul{
+  list-style-type: none;
+  font-size:0.8em;
+  padding-left: 0;
+  margin-left: 0;
+}
+li{
+  display: block;
+  text-align: left;
+  margin-top: 1em;
+  margin-bottom: 1em;
+  margin-left: 0.4em;
+  margin-right: 0;
+  color:red;
+  
+}
 </style>
